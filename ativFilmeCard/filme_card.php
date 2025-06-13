0a8846de7bf89c299322ce3e28305d8a4022244b
luiza <134729599+mlads15@users.noreply.github.com>
2025-06-13 14:52:04 -0300

    Add files via upload

---
+++ ativFilmeCard/filme_card.php
@@ -0,0 +1,14 @@
+<?php
+
+$nome = $_POST['nome'];
+$nomeDiretor = $_POST['nomeDiretor'];
+$anoEstreia = $_POST['anoEstreia'];
+$link = $_POST['img'];
+
+
+echo "<div style='border: solid 1px; width: 300px; margin-top: 20px;'>
+O Poderoso Chefão<br>
+ Francis Coppola<br>
+1972<br>
+ <img style='width: 100%; height: auto;' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKbcHr3lNglyLImgx1e23RlVgUdK3lxNK83Q&s' /><br>
+</div>";
